6cabb53832ed0892b0a2b828ac9226984ae6e0f0
Michel Mongkhoy <noreply>
2014-05-18 18:20:09 +0000

    Update components

--- ui/play.reel/play.js
+++ ui/play.reel/play.js
@@ -12,6 +12,21 @@ exports.Play = Component.specialize(/** @lends Play# */ {
     constructor: {
         value: function Play() {
             this.super();
+            
+            this.players = [];
+
+            var lcRoom1 = new Room();
+            lcRoom1.identifier = "room1";
+            lcRoom1.name = "# Room 1";
+            this.roomList.push(lcRoom1);
+
+            var lcRoom2 = new Room();
+            lcRoom2.identifier = "room2";
+            lcRoom2.name = "# Room 2";
+            this.roomList.push(lcRoom2);
         }
+    },
+    players: {
+        value: null
     }
 });
