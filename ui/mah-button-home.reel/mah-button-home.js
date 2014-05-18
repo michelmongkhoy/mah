5d22291898d23a58e3aeeb0dbd50c21eaddac1d8
Michel Mongkhoy <noreply>
2014-05-18 04:42:11 +0000

    Update components

--- ui/mah-button-home.reel/mah-button-home.js
+++ ui/mah-button-home.reel/mah-button-home.js
@@ -3,12 +3,13 @@
  * @requires montage/ui/component
  */
 var Component = require("montage/ui/component").Component;
+var AbstractButton = require("montage/ui/base/abstract-button").AbstractButton;
 
 /**
  * @class MahButtonHome
  * @extends Component
  */
-exports.MahButtonHome = Component.specialize(/** @lends MahButtonHome# */ {
+exports.MahButtonHome = AbstractButton.specialize(/** @lends MahButtonHome# */ {
     constructor: {
         value: function MahButtonHome() {
             this.super();
