cd4097a321eb3af562d0a1944433a0f631fcdc0c
Michel Mongkhoy <noreply>
2014-05-18 18:04:37 +0000

    Add component play

---
+++ ui/play.reel/play.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/play.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Play
+ * @extends Component
+ */
+exports.Play = Component.specialize(/** @lends Play# */ {
+    constructor: {
+        value: function Play() {
+            this.super();
+        }
+    }
+});
