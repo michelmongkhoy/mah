df71925086f2337aa05103bf53fb07b40dfface5
Michel Mongkhoy <noreply>
2014-05-18 08:34:07 +0000

    Update component ui/rooms.reel/rooms.js

--- ui/rooms.reel/rooms.js
+++ ui/rooms.reel/rooms.js
@@ -12,6 +12,19 @@ exports.Rooms = Component.specialize(/** @lends Rooms# */ {
     constructor: {
         value: function Rooms() {
             this.super();
+            
+            roomList = [];
+            
+            var lcRoom1 = new Room();
+            lcRoom1.identifier = "room1";
+            lcRoom1.name = "# Room 1";
+            roomList.push(lcRoom1);
+            
+            var lcRoom2 = new Room();
+            lcRoom2.identifier = "room2";
+            lcRoom2.name = "# Room 2";
+            roomList.push(lcRoom2);
+            
         }
     },
     roomList: {
