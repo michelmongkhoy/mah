4230e29805a09f1414e7514a5356ded7eb2afd1f
Michel Mongkhoy <noreply>
2014-05-18 18:03:47 +0000

    Add module play

---
+++ ui/play.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/play
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Play
+ * @extends Montage
+ */
+exports.Play = Montage.specialize(/** @lends Play# */ {
+    constructor: {
+        value: function Play() {
+            this.super();
+        }
+    }
+});
