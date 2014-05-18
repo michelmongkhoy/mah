1db50427d1ab65365f81f9858bd44e8bbdf62c9e
Michel Mongkhoy <noreply>
2014-05-18 16:12:03 +0000

    Add module song

---
+++ model/song.js
@@ -0,0 +1,16 @@
+/**
+ * @module model/song
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Song
+ * @extends Montage
+ */
+exports.Song = Montage.specialize(/** @lends Song# */ {
+    constructor: {
+        value: function Song() {
+            this.super();
+        }
+    }
+});
