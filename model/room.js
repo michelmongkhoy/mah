246edbab2ac2f630318462f883d669be283b19fc
Michel Mongkhoy <noreply>
2014-05-18 08:27:48 +0000

    Add module room

---
+++ model/room.js
@@ -0,0 +1,16 @@
+/**
+ * @module model/room
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Room
+ * @extends Montage
+ */
+exports.Room = Montage.specialize(/** @lends Room# */ {
+    constructor: {
+        value: function Room() {
+            this.super();
+        }
+    }
+});
