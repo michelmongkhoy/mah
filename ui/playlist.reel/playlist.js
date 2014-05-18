0f2de103c5d139ffdff952b6c395118216dfebab
Michel Mongkhoy <noreply>
2014-05-18 10:25:25 +0000

    Add component playlist

---
+++ ui/playlist.reel/playlist.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/playlist.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Playlist
+ * @extends Component
+ */
+exports.Playlist = Component.specialize(/** @lends Playlist# */ {
+    constructor: {
+        value: function Playlist() {
+            this.super();
+        }
+    }
+});
