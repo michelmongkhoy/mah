4c295189ab23d73e963541674339a37ebdcf2855
Michel Mongkhoy <noreply>
2014-05-18 18:05:16 +0000

    Add component mah-button-picture

---
+++ ui/mah-button-picture.reel/mah-button-picture.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/mah-button-picture.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MahButtonPicture
+ * @extends Component
+ */
+exports.MahButtonPicture = Component.specialize(/** @lends MahButtonPicture# */ {
+    constructor: {
+        value: function MahButtonPicture() {
+            this.super();
+        }
+    }
+});
