fa19ec51b9728ca1032a267388184dfbcb1eb687
Michel Mongkhoy <noreply>
2014-05-18 16:10:42 +0000

    Add module playlist

---
+++ model/playlist.js
@@ -0,0 +1,16 @@
+/**
+ * @module model/playlist
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Playlist
+ * @extends Montage
+ */
+exports.Playlist = Montage.specialize(/** @lends Playlist# */ {
+    constructor: {
+        value: function Playlist() {
+            this.super();
+        }
+    }
+});
