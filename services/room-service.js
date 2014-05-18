5b17c8594a3182657352c2a12b37c4c9a0b2d04f
Michel Mongkhoy <noreply>
2014-05-18 08:28:17 +0000

    Add module room-service

---
+++ services/room-service.js
@@ -0,0 +1,16 @@
+/**
+ * @module services/room-service
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class RoomService
+ * @extends Montage
+ */
+exports.RoomService = Montage.specialize(/** @lends RoomService# */ {
+    constructor: {
+        value: function RoomService() {
+            this.super();
+        }
+    }
+});
