4350db056c37998fd41cc8986201d6fe59edec8f
Michel Mongkhoy <noreply>
2014-05-18 16:34:52 +0000

    Update component ui/pick-songs.reel/pick-songs.js

--- ui/pick-songs.reel/pick-songs.js
+++ ui/pick-songs.reel/pick-songs.js
@@ -20,10 +20,32 @@ exports.PickSongs = Component.specialize(/** @lends PickSongs# */ {
             
             var lcPlaylist1 = new Playlist();
             lcPlaylist1.name = "Justin Bieber greatest hits";
+            
+            lcPlaylist1.songs = [];
+            
+            var lcSong11 = new Song();
+            lcSong11.name = "Baby";
+            lcPlaylist1.songs.push(lcSong11);
+            
+            var lcSong12 = new Song();
+            lcSong12.name = "Boyfriend";
+            lcPlaylist1.songs.push(lcSong12);  
+            
             this.playlists.push(lcPlaylist1);
             
             var lcPlaylist2 = new Playlist();
             lcPlaylist2.name = "The Beatles";
+            
+            lcPlaylist2.songs = [];
+
+            var lcSong21 = new Song();
+            lcPlaylist2.name = "Let It Be";
+            lcPlaylist2.songs.push(lcPlaylist2);
+
+            var lcSong22 = new Song();
+            lcSong22.name = "Yesterday";
+            lcPlaylist2.songs.push(lcSong22); 
+            
             this.playlists.push(lcPlaylist2);
             
         }
