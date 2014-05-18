892346df0afed10c338dfadd3c14f54c886ab7de
Michel Mongkhoy <noreply>
2014-05-18 10:24:25 +0000

    Add component pick-songs

---
+++ ui/pick-songs.reel/pick-songs.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/pick-songs.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class PickSongs
+ * @extends Component
+ */
+exports.PickSongs = Component.specialize(/** @lends PickSongs# */ {
+    constructor: {
+        value: function PickSongs() {
+            this.super();
+        }
+    }
+});
