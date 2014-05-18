938b42088a207cbef16c5a2453d30e0aa34bed8e
Michel Mongkhoy <noreply>
2014-05-18 07:12:19 +0000

    Add component rooms

---
+++ ui/rooms.reel/rooms.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/rooms.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Rooms
+ * @extends Component
+ */
+exports.Rooms = Component.specialize(/** @lends Rooms# */ {
+    constructor: {
+        value: function Rooms() {
+            this.super();
+        }
+    }
+});
