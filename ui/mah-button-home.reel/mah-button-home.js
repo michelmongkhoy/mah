edeb9b5c317f265e9536c45c1c0946ddae149bfe
Michel Mongkhoy <noreply>
2014-05-18 04:05:04 +0000

    Add component mah-button-home

---
+++ ui/mah-button-home.reel/mah-button-home.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/mah-button-home.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MahButtonHome
+ * @extends Component
+ */
+exports.MahButtonHome = Component.specialize(/** @lends MahButtonHome# */ {
+    constructor: {
+        value: function MahButtonHome() {
+            this.super();
+        }
+    }
+});
