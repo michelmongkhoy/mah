7e678f61e51eb6bae48058efcf70b8f3d8adaaf7
Michel Mongkhoy <noreply>
2014-05-18 18:38:35 +0000

    Update component ui/play.reel/play.js

--- ui/play.reel/play.js
+++ ui/play.reel/play.js
@@ -18,12 +18,12 @@ exports.Play = Component.specialize(/** @lends Play# */ {
             var lcPlayer1 = new Room();
             lcPlayer1.identifier = "player1";
             lcPlayer1.name = "# PLayer 1";
-            this.roomList.push(lcPlayer1);
+            this.players.push(lcPlayer1);
 
             var lcPlayer2 = new Room();
             lcPlayer2.identifier = "room2";
             lcPlayer2.name = "# Player 2";
-            this.roomList.push(lcPlayer2);
+            this.players.push(lcPlayer2);
         }
     },
     players: {
